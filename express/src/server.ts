16c795bceb2f2a6fc5310066e6cb2c368db5f004
Simon Törnqvist <simon.thornqvist99@gmail.com>
2024-02-24 12:32:20 +0000

    add basic express server

---
+++ express/src/server.ts
@@ -0,0 +1,13 @@
+import express, {Express, Request, Response} from 'express';
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response)=>{
+    res.send('Hello, this is Express + TypeScript');
+    console.log(req);
+});
+
+app.listen(port, ()=> {
+console.log(`[Server]: I am running at https://localhost:${port}`);
+});
