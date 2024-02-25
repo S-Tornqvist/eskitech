e517421b493b3e8e33a933b9a5f093c2191d90b6
Simon Törnqvist <simon.tornqvist@liu.se>
2024-02-25 14:02:15 +0100

    add dev start

--- express/src/server.ts
+++ express/src/server.ts
@@ -1,13 +1,13 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 const port = 3000;
 
-app.get('*', (req: Request, res: Response)=>{
-    res.send('Hello, this is Express + TypeScript');
-    console.log(req.method, req.url);
+app.get("*", (req: Request, res: Response) => {
+  res.send("Hello, this is Express + TypeScript");
+  console.log(req.method, req.url);
 });
 
-app.listen(port, ()=> {
-console.log(`[Server]: I am running at https://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`[Server]: I am running at https://localhost:${port}`);
 });
